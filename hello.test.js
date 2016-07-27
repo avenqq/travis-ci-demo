b9053410df8fd19ccb2a2bdd6329b8558af0b8e1
avenwu.wwb <avenwu.wwb@alibaba-inc.com>
2016-07-27 23:55:02 +0800

    test

--- hello.test.js
+++ hello.test.js
@@ -2,6 +2,15 @@
 
 var test = require('tape');
 var hello = require('./hello.js');
+var http = require('http');
+
+var server = http.createServer(function(res, req) {
+	
+});
+
+server.listen(8080, function() {
+	console.log('-------successfull---------');
+});
 
 test('hello\'s output', function (t) {
   t.equal(hello(), 'Hello World');
